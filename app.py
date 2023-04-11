38572581c57291c0182047eabac821b226e57ea8
Laidybird <predaveronica72@yahoo.com>
2023-04-11 09:28:19 +0000

    Add Delete functionlity

--- app.py
+++ app.py
@@ -181,6 +181,12 @@ def edit_category(category_id):
 
 
 
+@app.route("/delete_category/<category_id>")
+def delete_category(category_id):
+    mongo.db.categories.delete_one({"_id": ObjectId(category_id)})
+    flash("Category Successfully Deleted")
+    return redirect(url_for("get_categories"))
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
